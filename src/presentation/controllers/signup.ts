b72640d72cdba3f67f2e48e8c2ded553bfa25966
João Pedro PC <joao.santos@consultores.rnp.br>
2022-06-07 20:56:32 -0300

    refactor: ensure SignUpController.handle implement correct types

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,12 @@
+import { HttpRequest, HttpResponse } from "../protocols/http";
+
 export class SignUpController {
-  handle(httpRequest:any): any {
-    return {statusCode: 400,
-    body: new Error('Missing param: name')}
+  handle(httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.name) {
+      return { statusCode: 400, body: new Error("Missing param: name") };
+    }
+    if (!httpRequest.body.email) {
+      return { statusCode: 400, body: new Error("Missing param: email") };
+    }
   }
 }
