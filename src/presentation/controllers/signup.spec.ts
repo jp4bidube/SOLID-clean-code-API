a5c5ddb72f69411aeac082a7015504dfc209dc11
João Pedro PC <joao.santos@consultores.rnp.br>
2022-06-07 21:31:21 -0300

    refactor: move sut creation factor helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,45 +1,63 @@
 import { MissingParamError } from "../errors/missing-param-error";
 import { SignUpController } from "./signup";
 
-describe('SignUp Controller', () => {
-  test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController();
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
+describe("SignUp Controller", () => {
+  test("Should return 400 if no name is provided", () => {
+    const sut = makeSut();
     const httpRequest = {
       body: {
-        email: 'any_email.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password_confirmation'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('name'))
-  })
-  
-  test('Should return 400 if no password is provided', () => {
-    const sut = new SignUpController();
+        email: "any_email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password_confirmation",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("name"));
+  });
+  test("Should return 400 if no email is provided", () => {
+    const sut = makeSut();
     const httpRequest = {
       body: {
-        name: 'any_name',
-        email: 'any_email.com',
-        passwordConfirmation: 'any_password_confirmation'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('password'))
-  })
-  test('Should return 400 if no password confirmation is provided', () => {
-    const sut = new SignUpController();
+        name: "any_name.com",
+        password: "any_password",
+        passwordConfirmation: "any_password_confirmation",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("email"));
+  });
+  test("Should return 400 if no password is provided", () => {
+    const sut = makeSut();
     const httpRequest = {
       body: {
-        name: 'any_name',
-        email: 'any_email.com',
-        password: 'any_password'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
-  })
+        name: "any_name",
+        email: "any_email.com",
+        passwordConfirmation: "any_password_confirmation",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("password"));
+  });
+  test("Should return 400 if no password confirmation is provided", () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email.com",
+        password: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError("passwordConfirmation")
+    );
+  });
 });
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,7 +5,12 @@ import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ["name", "email", "password",'passwordConfirmation'];
+    const requiredFields = [
+      "name",
+      "email",
+      "password",
+      "passwordConfirmation",
+    ];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
